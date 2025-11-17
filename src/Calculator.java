7c9294f817824ac6020b8c5ea0b59fc808d4128f
bharath <tbns3573@gmail.com>
2025-11-17 13:09:32 +0530

    Added subtraction

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,8 +3,15 @@ public class Calculator {
 	public static int add(int a, int b) {
         return a + b;
     }
+	
+	public static int subtract(int a, int b) {
+	    return a - b;
+	}
+
 
     public static void main(String[] args) {
         System.out.println("Addition: " + add(10, 5));
+        System.out.println("Subtraction: " + subtract(10, 5));
+
     }
 }
